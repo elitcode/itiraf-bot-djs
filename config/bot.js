4e974e72b62d6dec355d18fc23d2a5c18b33f821
flexy.js <82216090+elitcode@users.noreply.github.com>
2024-02-11 00:04:36 +0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,14 +1,14 @@
 module.exports = {
-    token: "MTIwMzc0NjM5NjcxNzU4ODU2Mg.GGv020.SEU12Wg6rkL1GtujImvPJS0Qymg_wyZLVxFHOk",
+    token: "BOT TOKEN",
 
     prefix: "!", // doldurmasanda olur
     botStatus: "1flexy",
 
-    ownerElitCode: "720903865876217869", // owner id
-    clientID:"1203746396717588562", // bot id
+    ownerElitCode: "id", // owner id
+    clientID:"id", // bot id
 
-    channelID:"1205627959671525478", // bot mesajı gönderileceği kanal
-    logChannelID:"1205628000855396352", // itirafların gönderileceği kanal!
+    channelID:"id", // bot mesajı gönderileceği kanal
+    logChannelID:"id", // itirafların gönderileceği kanal!
 
     gorsel:'https://media.licdn.com/dms/image/D4D03AQF4D3fKJQ-dug/profile-displayphoto-shrink_200_200/0/1685553662258?e=2147483647&v=beta&t=LqB0f6VnARyQ2YoLqeYC_Uz0Ivnj9jsQZHewwF_s3bM'
 }
